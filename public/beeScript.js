98d0a8a3eb9f135e91d1927ee50f0529a481f045
witb_world <e.c.wald@gmail.com>
2020-05-17 20:11:52 -0700

    Updated user interface

--- public/beeScript.js
+++ public/beeScript.js
@@ -2,26 +2,25 @@ const wordsFile = 'bee_list.txt';
 const lettersFile = 'letters2.json';
 var centerLetter = '';
 var letterSet = new Set();
-// var letterSet = letters.split("");
 let wordsArray = [];
 let guessedWords = [];
 let maxPoints = 0;
 let wordsList = [];
 let thisDate = new Date();
-thisDate.setHours(0,0,0,0);
+thisDate.setHours(0, 0, 0, 0);
 let dateKey = thisDate.toJSON();
 
 
-function getWordsText () {
+function getWordsText() {
     return $.ajax({
         'url': wordsFile
     });
 }
 
-function getLetters () {
+function getLetters() {
     return $.ajax({
         'url': lettersFile,
-        'dataType':'json'
+        'dataType': 'json'
     });
 }
 
@@ -31,52 +30,56 @@ function handleWordsData(data) {
     wordsList = getWordList(wordsArray);
     maxPoints = getMaxPoints(wordsList);
 
-    displayCenterLetter();
+    // displayCenterLetter();
     shuffleDisplay();
 }
 
 function handleLettersData(data) {
     let letterSets = data.letterSets;
     let i = 0;
-    for (letterSet of letterSets){
-        if (letterSet.date.valueOf() == dateKey.valueOf()){
+    for (letterSet of letterSets) {
+        if (letterSet.date.valueOf() == dateKey.valueOf()) {
             break;
         } else {
-            i ++;
+            i++;
         }
     }
     centerLetter = letterSets[i].magicLetter;
     letterSet = new Set(letterSets[i].letters);
 }
 
+function stripWhitespace(textStr) {
+
+}
+
 function parseGuess() {
     let valid = true;
-    let thisGuess = getGuess().toLowerCase();
+    let thisGuess = getGuess().toLowerCase().replace(/\s/g, '');
     let msg = "";
 
-    if (! validateforAlpha(thisGuess)) {
+    if (!validateforAlpha(thisGuess)) {
         valid = false;
         msg = "Alphabetical characters only please!"
-    } else if (thisGuess.length < 4){
+    } else if (thisGuess.length < 4) {
         valid = false;
         msg = "Guess must be at least four letters";
-    } else if (! lettersInSet(thisGuess)) {
+    } else if (!lettersInSet(thisGuess)) {
         valid = false;
         msg = "You must use the letters in the set";
-    } else if (! thisGuess.includes(centerLetter)) {
+    } else if (!thisGuess.includes(centerLetter)) {
         valid = false;
         msg = "Your guess must include the center letter";
     } else if (guessedWords.includes(thisGuess)) {
         valid = false;
         msg = "You already guessed this word";
-    } else if(! wordsList.includes(thisGuess)){
+    } else if (!wordsList.includes(thisGuess)) {
         valid = false;
         msg = "this word is not in the set :(";
     }
 
     if (valid) {
         msg = isPangram(thisGuess) ? "Pangram! Way to go!" : "Good guess!";
-        
+
         updatePoints(thisGuess);
         guessedWords.push(thisGuess);
         updateGuesses(thisGuess);
@@ -85,9 +88,9 @@ function parseGuess() {
 }
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--){
+    for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        
+
         [array[i], array[j]] = [array[j], array[i]]
         // console.log(array);
     }
@@ -100,9 +103,9 @@ function displayCenterLetter() {
 function shuffleDisplay() {
     document.getElementById("letterSet").innerHTML = "";
     letterArray = shuffle(Array.from(letterSet));
-    for (let letter of letterArray){
+    for (let letter of letterArray) {
         if (letter === centerLetter) {
-            document.getElementById("letterSet").innerHTML += `<font color="yellow">${letter} </font>`;
+            document.getElementById("letterSet").innerHTML += `<font color="gold">${letter} </font>`;
         } else {
             document.getElementById("letterSet").innerHTML += letter + " ";
         }
@@ -114,34 +117,40 @@ function updatePoints(guess) {
     currentPoints += awardPoints(guess);
     let rank = "Let's play!";
     let rankVal = currentPoints / maxPoints;
-
-    if (rankVal > 0.20) {
-        rank = "Genius!";
-    } else if (rankVal > 0.10) {
-        rank = "Amazing!";
-    } else if (rankVal > 0.05 ) {
-        rank = "Niiiiiiice";
-    } else if (rankVal > 0.025) {
-        rank = "Solid.";
-    } else if (rankVal > .008) {
-        rank = "Making progress!";
-    } else if (rankVal > .004) {
-        rank = "Moving up!";
-    } else if (rankVal > .002) {
-        rank = "Good start!"
+    let genius = maxPoints;
+    let ranks = [{ rank: "Genius!", points: Math.floor(maxPoints * 0.20) },
+    { rank: "Amazing!", points: Math.floor(maxPoints * 0.10) },
+    { rank: "Niiiiice", points: Math.floor(maxPoints * 0.05) },
+    { rank: "Solid.", points: Math.floor(maxPoints * 0.025) },
+    { rank: "Making progress!", points: Math.floor(maxPoints * 0.008) },
+    { rank: "Moving up!", points: Math.floor(maxPoints * 0.004) },
+    { rank: "Good start!", points: Math.floor(maxPoints * 0.002) }];
+    let levelBarValue = currentPoints / ranks[0].points;
+    console.log(Math.floor(levelBarValue * 100));
+    let nextRank = ranks[6];
+    let i = 0;
+    for (ranking of ranks) {
+        if (currentPoints >= ranking.points) {
+            rank = ranking.rank;
+            break;
+        }
+        i++;
+        nextRank = ranks[i - 1];
     }
 
+    document.getElementById("pbar").style.width = Math.floor(levelBarValue * 100) + "%";
+    document.getElementById("nextRank").innerHTML = `${nextRank.points - currentPoints} points to level up!`
     document.getElementById("points").innerHTML = currentPoints;
-    document.getElementById("ranking").innerHTML = rank;
+    document.getElementById("level").innerHTML = rank;
 }
 
 function updateGuesses(guess) {
     document.getElementById("guesses").innerHTML = formatGuesses(guessedWords);
 }
 
-function formatGuesses(guessedWords){
+function formatGuesses(guessedWords) {
     let guessedWordsFormatted = '<ul class="list-inline">';
-    for (word of guessedWords){
+    for (word of guessedWords) {
         guessedWordsFormatted += `<li class="list-inline-item"><a href="https://www.dictionary.com/browse/${word}" target="_blank">${word}</a></li>`
     }
     guessedWordsFormatted += '</ul>';
@@ -196,14 +205,14 @@ function getWordList(wordsArray) {
 }
 
 function lettersInSet(word) {
-    for (letter of word.split('')){
+    for (letter of word.split('')) {
         if (!letterSet.has(letter)) return false;
     }
     return true;
 }
 
 function isPangram(word) {
-    for (letter of letterSet){
+    for (letter of letterSet) {
         if (!word.includes(letter)) return false;
     }
     return true;
